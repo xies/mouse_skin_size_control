c5ad0362f78e196db0a0244b7b1916813f8ac2a3
Shicong Xie <xies@users.noreply.github.com>
2021-05-25 02:58:33 -0700

    NMS preprocessing scripts

---
+++ nms_analysis/preprocessing_scripts/05_03_21_rb_fl_normalize.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 13:02:36 2021
+
+@author: xies
+"""
+
+import numpy as np
+from skimage import io, util
+from os import path
+
+dirname = '/Users/xies/Box/Mouse/Skin/Two photon/NMS/05-03-2021 Rb-fl/M2 RB-KO/R1/stitched'
+
+
+#%%
+im = io.imread(path.join(dirname,'KO_R1.tif'))
+
+R = im[:,:,0,:,:] #FUCCI
+G = im[:,:,1,:,:] #H2B
+
+#%% Normalize
+
+R_ = R.copy().astype(np.float64)
+for t,Rslice in enumerate(R):
+    Rslice = Rslice/Rslice.mean()
+    Rslice = Rslice - 500
+    R_[t,:,:,:] = Rslice
+    
+G_ = G.copy().astype(np.float64)
+for t,Gslice in enumerate(G):
+    
+    Gslice = Gslice/Gslice.mean()
+    Gslice = Gslice - 500
+    G_[t,:,:,:] = Gslice
+
+#%%
+
+io.imsave(path.join(dirname,'KO_R1_Rc.tif'),util.img_as_uint(R_/R_.max()))
+io.imsave(path.join(dirname,'KO_R1_Gc.tif'),util.img_as_uint(G_/G_.max()))
+
