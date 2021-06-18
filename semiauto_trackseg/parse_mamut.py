06efb6546a1b4be2a7bd32a1b1fc0099ac382218
Shicong Xie <xies@stanford.edu>
2021-06-18 16:36:37 -0700

    mamut cycle extractor working

--- semiauto_trackseg/parse_mamut.py
+++ semiauto_trackseg/parse_mamut.py
@@ -3,6 +3,12 @@
 """
 Created on Thu Jun 17 14:58:19 2021
 
+Parses .csv output of Mamut and prunes out complete cell cycles
+
+Exports (pickle) as a list of dataframes, each corresponding to a complete cycle (from birth to division).
+Exported fields:
+    SpotID  X    Y    Z    T     Left child      Right child    Division(flag)     Terminus (flag)
+
 @author: xies
 """
 
@@ -12,6 +18,8 @@ import matplotlib.pylab as plt
 
 import seaborn as sb
 from os import path
+
+import pickle as pkl
 
 # Avoid parsing XML
 # import xml.etree.ElementTree as ET
@@ -41,62 +49,77 @@ for tID in cycling_tracks['TRACK_ID'].values:
 
 #NB: Spots and links are chronologically sorted (descending)
 
-link = cycling_links[0]
-spots_ = cycling_spots[0]
-
-spots = pd.DataFrame()
-# Construct a cleaned-up dataframe
-spots['ID'] = spots_['ID']
-spots['X'] = spots_['POSITION_X']
-spots['Y'] = spots_['POSITION_Y']
-spots['Z'] = spots_['POSITION_Z']
-spots['T'] = spots_['POSITION_T']
-spots['Left'] = None
-spots['Right'] = None
-spots['Division'] = False
-spots['Terminus'] = False
-
-
-# Build a daughter(s) tree into the spots dataframe
-
-for idx,spot in spots.iterrows():
-    links_from_this_spot = link[link['SPOT_SOURCE_ID'] == spot.ID]
+tracks = []
+for i in range(len(cycling_tracks)):
     
-    if len(links_from_this_spot) == 1:
-        # No division
-        spots.at[idx,'Left'] = links_from_this_spot.iloc[0]['SPOT_TARGET_ID']
-    elif len(links_from_this_spot) == 2:
-        # There are two daughters
-        spots.at[idx,'Left'] = links_from_this_spot.iloc[0]['SPOT_TARGET_ID']
-        spots.at[idx,'Right'] = links_from_this_spot.iloc[1]['SPOT_TARGET_ID']
-        spots.at[idx,'Division'] = True
-    elif len(links_from_this_spot) == 0:
-        spots.at[idx,'Terminus'] = True
+    link = cycling_links[i]
+    spots_ = cycling_spots[i]
+    
+    spots = pd.DataFrame()
+    # Construct a cleaned-up dataframe
+    spots['ID'] = spots_['ID']
+    spots['X'] = spots_['POSITION_X']
+    spots['Y'] = spots_['POSITION_Y']
+    spots['Z'] = spots_['POSITION_Z']
+    spots['T'] = spots_['POSITION_T']
+    spots['Left'] = None
+    spots['Right'] = None
+    spots['Division'] = False
+    spots['Terminus'] = False
+    
+    
+    # Build a daughter(s) tree into the spots dataframe
+    
+    for idx,spot in spots.iterrows():
+        links_from_this_spot = link[link['SPOT_SOURCE_ID'] == spot.ID]
+        
+        if len(links_from_this_spot) == 1:
+            # No division
+            spots.at[idx,'Left'] = links_from_this_spot.iloc[0]['SPOT_TARGET_ID']
+        elif len(links_from_this_spot) == 2:
+            # There are two daughters
+            spots.at[idx,'Left'] = links_from_this_spot.iloc[0]['SPOT_TARGET_ID']
+            spots.at[idx,'Right'] = links_from_this_spot.iloc[1]['SPOT_TARGET_ID']
+            spots.at[idx,'Division'] = True
+        elif len(links_from_this_spot) == 0:
+            spots.at[idx,'Terminus'] = True
+    
+    # From first cell division, initiate a Track object and traverse until we hit either: 1) division or 2) terminus
+    # If Division, complete current Track and try to initiate another Track object
+    # If Terminus, delete current Track object
+    
+    divisions = spots[ spots['Division'] == True ]
+    first_division = divisions.sort_values('T').iloc[0]
+    
+    daughter_a = spots[spots['ID'] == first_division['Left']]
+    daughter_b = spots[spots['ID'] == first_division['Right']]
+    
+    tracks_ = []
+    
+    spots2trace = [daughter_a, daughter_b]
+    while len(spots2trace) > 0:
+    
+        # Pop from list
+        spot = spots2trace.pop()
+        print(f'Tracing from {spot.ID}')
+        track = [spot]
+        while not spot.iloc[0]['Division'] and not spot.iloc[0]['Terminus']:
+            # Trace the linkages
+            next_spot = spots[spots.ID == spot['Left'].iloc[0]]
+            track.append(next_spot)
+            spot = next_spot
+        if spot.iloc[0]['Division']:
+            # If we found a division, then this is a complete cell cycle
+            tracks_.append(pd.concat(track))
+            daughter_a = spots[spots['ID'] == spot.iloc[0]['Left']]
+            daughter_b = spots[spots['ID'] == spot.iloc[0]['Right']]
+            # Append the new daughters into the todo list
+            spots2trace.extend([daughter_a, daughter_b])
+    
+    tracks.extend(tracks_)
 
-# From first cell division, initiate a Track object and traverse until we hit either: 1) division or 2) terminus
-# If Division, complete current Track and try to initiate another Track object
-# If Terminus, delete current Track object
 
-divisions = spots[ spots['Division'] == True ]
-first_division = divisions.sort_values('T').iloc[0]
-
-daughter_a = spots[spots['ID'] == first_division['Left']]
-daughter_b = spots[spots['ID'] == first_division['Right']]
-
-complete_tracks = pd.DataFrame()
-for
-
-
-
-# # Find the split points
-# I = np.zeros_like(link['Label'],dtype=bool)
-# x = link['SPOT_SOURCE_ID']
-# I[np.unique(x, return_index=True)[1]] = True
-# splitting_source_IDs = x[~I]
-
-# # From split points, build dictionary of daughters
-# split_targets = { sourceID: link[link['SPOT_SOURCE_ID'] == sourceID]['SPOT_TARGET_ID'].values 
-#                  for sourceID in splitting_source_IDs}
-
-
+#%% Export the coordinates of the completed cell cycles (as pickle)
+with open(path.join(dirname,'complete_cycles.pkl'),'wb') as file:
+    pkl.dump(tracks,file)
 
