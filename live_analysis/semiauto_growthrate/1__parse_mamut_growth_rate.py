5ff7edcc7081b5b4f78c361a87c676bff1c9edb3
Shicong Xie <xies@stanford.edu>
2022-03-01 16:36:01 -0800

    parser for partial tracks/growth rate analysis

--- live_analysis/semiauto_growthrate/1__parse_mamut_growth_rate.py
+++ live_analysis/semiauto_growthrate/1__parse_mamut_growth_rate.py
@@ -7,7 +7,7 @@ Parses .csv output of Mamut and quantify growth rates
 
 Exports (pickle) as a list of dataframes, each corresponding to a complete cycle (from birth to division).
 Exported fields:
-    SpotID  X    Y    Z    T     Left child      Right child    Division(flag)     Terminus (flag)
+    SpotID  X    Y    Z    T     Left child      Right child    Division(flag)     Terminus (flag)  Source (flag)
 
 @author: xies
 """
@@ -34,10 +34,7 @@ raw_spots['TRACK_ID'] = raw_spots['TRACK_ID'].astype(int)
 raw_links = pd.read_csv(path.join(dirname,'MaMuT/linkage.csv'),skiprows=[1,2,3],header=0)
 raw_tracks = pd.read_csv(path.join(dirname,'MaMuT/tracks.csv'),skiprows=[1,2,3],header=0)
 
-# Do pre-filtering
-# Filter out tracks with fewer than 2 splits (i.e. no complete cell cycles)
-cycling_tracks = raw_tracks[raw_tracks['NUMBER_SPLITS'] > 1]
-num_tracks = len(cycling_tracks)
+num_tracks = len(raw_tracks)
 
 def sort_links_by_time(links,spots):
     for idx,link in links.iterrows():
@@ -54,16 +51,103 @@ def sort_links_by_time(links,spots):
 
 raw_links = sort_links_by_time(raw_links,raw_spots)
 
-
 # Separate all spots/linkage into lists of dfs pertaining to only a single track
-cycling_spots = []
-cycling_links = []
-for tID in cycling_tracks['TRACK_ID'].values:
-    cycling_spots.append(raw_spots[raw_spots['TRACK_ID'] == tID])
-    cycling_links.append(raw_links[raw_links['TRACK_ID'] == tID])
+tmp_spots = []
+tmp_links = []
+for tID in raw_tracks['TRACK_ID'].values:
+    tmp_spots.append(raw_spots[raw_spots['TRACK_ID'] == tID])
+    tmp_links.append(raw_links[raw_links['TRACK_ID'] == tID])
 
+
+# tmp_tracks = []
+spots = []
+
+# Parse each lineage tree and identify SOURCE and TERMINUS spots
+for i in range(num_tracks):
+    
+    link = tmp_links[i]
+    this_spots_ = tmp_spots[i].sort_values(by='POSITION_T')
+        
+    this_spots = pd.DataFrame()
+    # Construct a cleaned-up dataframe
+    this_spots['ID'] = this_spots_['ID']
+    this_spots['X'] = this_spots_['POSITION_X']
+    this_spots['Y'] = this_spots_['POSITION_Y']
+    this_spots['Z'] = this_spots_['POSITION_Z']
+    this_spots['Frame'] = this_spots_['POSITION_T']
+    this_spots['TrackID'] = this_spots_['TRACK_ID']
+    this_spots['Left'] = None
+    this_spots['Right'] = None
+    this_spots['Division'] = False
+    this_spots['Terminus'] = False
+    this_spots['Source'] = False
+    
+    # The first spots (all spots should be sorted by frame) and all subsequent result of splits are 'sources'
+    this_spots.at[this_spots.index[0],'Source'] = True
+
+    # Build a daughter(s) tree into the spots dataframe
+    # Notes: COULD build this on the first pass-through, but it's probably safer to build a temporary database and further split
+    for idx,spot in this_spots.iterrows():
+        links_from_this_spot = link[link['SPOT_SOURCE_ID'] == spot.ID]
+        
+        if len(links_from_this_spot) == 1:
+            # No division
+            this_spots.at[idx,'Left'] = links_from_this_spot.iloc[0]['SPOT_TARGET_ID']
+        elif len(links_from_this_spot) == 2:
+            # There are two daughters
+            this_spots.at[idx,'Left'] = links_from_this_spot.iloc[0]['SPOT_TARGET_ID']
+            this_spots.at[idx,'Right'] = links_from_this_spot.iloc[1]['SPOT_TARGET_ID']
+            this_spots.at[idx,'Division'] = True
+            
+            this_spots.at[ this_spots[this_spots['ID'] == this_spots.loc[idx]['Left']].index[0],'Source'] = True
+            this_spots.at[ this_spots[this_spots['ID'] == this_spots.loc[idx]['Right']].index[0],'Source'] = True
+            
+            
+        elif len(links_from_this_spot) == 0:
+            this_spots.at[idx,'Terminus'] = True
+    
+    spots.append(this_spots)
+
+
+#%% Separate mother / daughter tracks
+
+tracks = []
+
+for i,this_spots in enumerate(spots):
+    
+    link = tmp_links[i]
+    assert(sum(this_spots['Source']) > 0)
+    
+    # If only a single SOURCE then it's a linear track
+    if sum(this_spots['Source']) == 1:
+        tracks.append(this_spots)
+        
+    # If there are splits
+    else:
+        sources = this_spots[this_spots['Source'] & ~this_spots['Terminus'] & ~this_spots['Division']]
+        for _,source in sources.iterrows():
+
+            links_from_this_spot = link[link['SPOT_SOURCE_ID'] == source.ID]
+            assert(len(links_from_this_spot) == 1) # should only be one outgoing track
+            
+            TERMINATED = False
+            track_ = []
+            track_.append(source)
+            while not TERMINATED:
+                
+                next_spot = this_spots[ this_spots['ID'] == links_from_this_spot['SPOT_TARGET_ID'].iloc[0]].iloc[0]
+                source = next_spot
+                links_from_this_spot = link[link['SPOT_SOURCE_ID'] == source.ID]
+                
+                track_.append(next_spot)
+                if next_spot['Terminus']:
+                    TERMINATED = True
+            
+            track_ = pd.DataFrame(track_)
+            tracks.append(track_)
 
 #%% Export the coordinates of the completed cell cycles (as pickle)
-with open(path.join(dirname,'MaMuT/complete_cycles.pkl'),'wb') as file:
+with open(path.join(dirname,'MaMut/tracks.pkl'),'wb') as file:
     pkl.dump(tracks,file)
 
+ 
