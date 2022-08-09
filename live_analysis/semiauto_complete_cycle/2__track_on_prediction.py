197890d328aa00d2cc671ecb4d686337bf3d7286
Shicong Xie <xies@stanford.edu>
2022-08-09 12:39:43 -0700

    renamed stardist->prediction

--- live_analysis/semiauto_complete_cycle/2__track_on_prediction.py
+++ live_analysis/semiauto_complete_cycle/2__track_on_prediction.py
@@ -18,7 +18,8 @@ from glob import glob
 
 import pickle as pkl
 
-dirname = '/Users/xies/Box/Mouse/Skin/Two photon/NMS/10-20-2021/WT/R1'
+dirnames = {}
+dirname = '/Users/xies//OneDrive - Stanford/Skin/06-25-2022/M6 RBKO/R1/manual_track'
 
 # dx = 0.2920097
 dx = 1
@@ -30,8 +31,8 @@ with open(path.join(dirname,'MaMuT','complete_cycles.pkl'),'rb') as file:
     tracks = pkl.load(file)
 
 # Load prediction by stardist
-filenames = [path.join(dirname,f'reg/prediction/z_reg_t{t}_chan2.tif') for t in range(19)]
-seg = np.array([ io.imread(f) for f in filenames ])
+# filenames = [path.join(dirname,f'reg/prediction/z_reg_t{t}_chan2.tif') for t in range(19)]
+# seg = np.array([ io.imread(f) for f in filenames ])
 
 # Load prediction by stardist
 filenames = [path.join(dirname,f'reg/prediction/z_reg_t{t}_chan2.tif') for t in range(19)]
