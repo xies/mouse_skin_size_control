bf68036f842cf5a2ffba686a504071710339819e
Shicong Xie <xies@stanford.edu>
2023-06-04 00:22:04 -0700

    working on tracking: export ground truth to networkx

---
+++ live_analysis/misc_scripts/export_segmentation_as_csv.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 24 21:26:50 2023
+
+@author: xies
+"""
+
+import numpy as np
+import pandas as pd
+from skimage import io, measure
+from os import path
+
+from tqdm import tqdm
+
+dirname = '/Users/xies/OneDrive - Stanford/Skin/Mesa et al/W-R1/'
+
+#%%
+
+T = 15
+
+_tmp = []
+for t in tqdm(range(T)):
+    
+    im = io.imread(path.join(dirname,f'3d_nuc_seg/cellpose_cleaned_manual/t{t}.tif'))
+    
+    df = pd.DataFrame(measure.regionprops_table(im,properties=['label','centroid']))
+    df['T'] = t
+    
+    df = df.rename(columns={'centroid-0':'Z','centroid-1':'Y','centroid-2':'X','label':'CellposeID'})
+    df['Y'] /= 4
+    df['X'] /= 4
+    
+    _tmp.append(df)
+    
+df = pd.concat(_tmp,ignore_index=True)
+
+df.to_csv(path.join(dirname,'3d_nuc_seg/cellpose_cleaned_manual/zyxt.csv'))
+
