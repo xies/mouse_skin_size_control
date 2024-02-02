2c103e35f1a34037a6a81ed304cca344ff6ff915
Shicong Xie <xies@users.noreply.github.com>
2024-02-02 11:11:40 -0800

    cleaned up code for flattening -> fixed typos

--- live_analysis/flatten_tissue/1__flatten_using_channel.py
+++ live_analysis/flatten_tissue/1__flatten_using_channel.py
@@ -31,7 +31,7 @@ SIGN = -1
 #%%
 
 XY_sigma = 25
-Z_sigma = 10
+Z_sigma = 20
 
 TOP_Z_BOUND = 35
 BOTTOM_Z_BOUND = 75
@@ -54,20 +54,7 @@ for t in tqdm(range(imstack.shape[0])):
         continue
     
     im_z_blur = gaussian_filter(im,sigma=[Z_sigma,XY_sigma,XY_sigma])
-    # im_xy_blur = np.zeros_like(im[:,:,:],dtype=float)
-    #XY_blur
-    # for z,im_ in enumerate(im[:,:,:]):
-    #     im_xy_blur[z,...] = filters.gaussian(im_,sigma = XY_sigma) 
-    
-        
-    # #Z_blur
-    # im_z_blur = np.zeros_like(im_xy_blur)
-    # for x in range(XX):
-    #     for y in range(XX):
-    #         im_z_blur[:,y,x] = filters.gaussian(im_xy_blur[:,y,x], sigma= Z_sigma)
-            
-    # io.imsave(path.join(dirname,f'Image flattening/xyz_blur/t{t}.tif'), util.img_as_int(im_z_blur),check_contrast=False)
-    
+
     
     _tmp = im_z_blur.copy().astype(float)
     # Derivative of R_sgh wrt Z -> Take the max dI/dz for each (x,y) position
@@ -77,23 +64,22 @@ for t in tqdm(range(imstack.shape[0])):
     heightmap[heightmap > BOTTOM_Z_BOUND] = BOTTOM_Z_BOUND
     heightmap[heightmap < TOP_Z_BOUND] = TOP_Z_BOUND
     
-    io.imsave(path.join(dirname,f'Image flattening/heightmaps/t{t}.tif'), heightmap.astype(np.uint16),check_contrast=False)
     
     # Reconstruct flattened movie
     Iz = np.round(heightmap + z_shift).astype(int)
     
-    # NB: tried using np,take and np.choose, doesn't work bc of size limit. DO NOT use np.take
+    # NB: tried using np.take and np.choose, doesn't work bc of size limit. DO NOT use np.take
     flat = np.zeros((XX,XX))
     height_image = np.zeros_like(im)
     for x in range(XX):
         for y in range(XX):
             flat[y,x] = im[Iz[y,x],y,x]
             height_image[Iz[y,x],y,x] = 1
-    
+            
+    # io.imsave(path.join(dirname,f'Image flattening/xyz_blur/t{t}.tif'), util.img_as_int(im_z_blur),check_contrast=False)
+    io.imsave(path.join(dirname,f'Image flattening/heightmaps/t{t}.tif'), heightmap.astype(np.uint16),check_contrast=False)
     # io.imsave(path.join(dirname,f'Image flattening/flat_z_shift_{z_shift}/t{t}.tif'), flat.astype(np.int16),check_contrast=False)
     io.imsave(path.join(dirname,f'Image flattening/height_image/t{t}.tif'), height_image.astype(np.int16),check_contrast=False)
-    # io.imsave(path.join(dirname,f'R1_height_img.tif'), height_image.astype(np.int16),check_contrast=False)
-
     
     pd.Series({'XY_sigma':XY_sigma,'Z_sigma':Z_sigma,'TOP_Z_BOUND':TOP_Z_BOUND,'BOTTOM_Z_BOUND':BOTTOM_Z_BOUND,
               'z_shift':z_shift}).to_csv(path.join(dirname,f'Image flattening/params/t{t}.csv'))
