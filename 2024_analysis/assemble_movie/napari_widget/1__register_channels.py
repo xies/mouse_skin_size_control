5e6e35d4357bd7b202a08840792f7ce85e5e645b
Shicong Xie <xies@users.noreply.github.com>
2024-05-03 10:24:41 -0700

    small bug in napari gui text

--- 2024_analysis/assemble_movie/napari_widget/1__register_channels.py
+++ 2024_analysis/assemble_movie/napari_widget/1__register_channels.py
@@ -31,7 +31,7 @@ from re import findall
 
 # Default choices for the timepoints to insepct
 global DEFAULT_CHOICES
-DEFAULT_CHOICES = [0,1,2]
+DEFAULT_CHOICES = [None]
 
 #%% Reading the first ome-tiff file using imread reads entire stack
 
@@ -41,11 +41,7 @@ def sort_by_slice(filename):
     return int(z)
 
 
-@magicgui(call_button='Print size')
-def print_image_size(image : Image):
-    show_info(f'Size of {image.name} = {image.data.shape}')
 
-    
 '''
 Step one: Load all the OME-TIFFs and re-save as multipage TIFFs for each time point in region
 Will perform a StackReg on each stack just in case there is small movement.
@@ -56,9 +52,11 @@ Will perform a StackReg on each stack just in case there is small movement.
 def load_ome_tiffs_and_save_as_tiff(
     dirname=Path('/some/path.ext'),
     pattern_str: str='*/ZSeries*/',
-    OVERWRITE: bool=False
+    OVERWRITE: bool=False,
+    register_G: bool=True,
+    register_R: bool=True,
     ):
-    
+
     subfolders = glob(path.join(dirname,pattern_str))
 
     header_ome_h2b = []
@@ -77,13 +75,13 @@ def load_ome_tiffs_and_save_as_tiff(
 
     # Register the B/G channels (using B as reference)
     for header_ome in progress(header_ome_h2b):
-        
+
         d = path.split(path.dirname(header_ome))[0]
         # Make sure we haven't already processed this stack
         if path.exists(path.join(d,'G_reg.tif')) and not OVERWRITE:
             print(f'Skipping {d}')
             continue
-        
+
         # Load ome-tif
         print(f'Loading {d}')
         stack = io.imread(header_ome,is_ome=True)
@@ -92,48 +90,56 @@ def load_ome_tiffs_and_save_as_tiff(
             B = stack[1,...]
         else:
             B = stack
-        
-        # Use StackReg
-        print(f'Registering {d}')
-        sr = StackReg(StackReg.TRANSLATION) # There should only be slight sliding motion within a single stack
-        T = sr.register_stack(B,reference='previous',n_frames=20,axis=0) #Obtain the transformation matrices
-        B_reg = sr.transform_stack(B,tmats=T) # Apply to both channels
-        G_reg = sr.transform_stack(G,tmats=T)
-        
+
+        if register_G:
+            # Use StackReg
+            print(f'Registering {d}')
+            sr = StackReg(StackReg.TRANSLATION) # There should only be slight sliding motion within a single stack
+            T = sr.register_stack(B,reference='previous',n_frames=20,axis=0) #Obtain the transformation matrices
+            B_reg = sr.transform_stack(B,tmats=T) # Apply to both channels
+            G_reg = sr.transform_stack(G,tmats=T)
+        else:
+            B_reg = B
+            G_reg = G
+
         output_path = path.join( d,'B_reg.tif')
         io.imsave(output_path,util.img_as_uint(B_reg/B_reg.max()),check_contrast=False)
         output_path = path.join( d,'G_reg.tif')
         io.imsave(output_path,util.img_as_uint(G_reg/G_reg.max()),check_contrast=False)
-        
+
         print(f'Saved with {output_path}')
 
     # Register the R/R_shg channels (using R as reference)
     for header_ome in progress(header_ome_fucci):
-    
+
         d = path.split(path.dirname(header_ome))[0]
         # Make sure we haven't already processed this stack
         if path.exists(path.join(d,'R_reg.tif')) and not OVERWRITE:
             print(f'Skipping {d}')
             continue
-        
+
         # Load ome-tif
         print(f'Loading {d}')
         stack = io.imread(header_ome)
         R = stack[0,...]
         R_shg = stack[1,...]
-        
+
         # Use StackReg
-        print(f'Registering {d}')
-        sr = StackReg(StackReg.TRANSLATION) # There should only be slight sliding motion within a single stack
-        T = sr.register_stack(R,reference='previous',axis=0) #Obtain the transformation matrices
-        R_reg = sr.transform_stack(R,tmats=T) # Apply to both channels
-        R_shg_reg = sr.transform_stack(R_shg,tmats=T) # Apply to both channels
-        
+        if register_R:
+            print(f'Registering {d}')
+            sr = StackReg(StackReg.TRANSLATION) # There should only be slight sliding motion within a single stack
+            T = sr.register_stack(R,reference='previous',axis=0) #Obtain the transformation matrices
+            R_reg = sr.transform_stack(R,tmats=T) # Apply to both channels
+            R_shg_reg = sr.transform_stack(R_shg,tmats=T) # Apply to both channels
+        else:
+            R_reg = R
+            R_shg_reg = R_shg
+
         output_path = path.join( d,'R_reg.tif')
         io.imsave(output_path,util.img_as_uint(R_reg/R_reg.max()),check_contrast=False)
         output_path = path.join( d,'R_shg_reg.tif')
         io.imsave(output_path,util.img_as_uint(R_shg_reg/R_shg_reg.max()),check_contrast=False)
-        
+
         print(f'Saved with {output_path}')
 
 
@@ -144,18 +150,18 @@ def _update_timepoints_on_file_change(widget):
     """Called whenever the file picker is changed. Will look into that directory,
     and return the available timepoints as defined by parse_unregistered_channels
     """
-    
+
     dirname = widget.dirname.value
     choices = None
     filelist = parse_unregistered_channels(dirname)
-    
+
     if len(filelist) > 0:
         choices = filelist.index
     else:
         show_warning(f'Directory {dirname} is not a region directory.')
     if choices is not None:
         widget.timepoints_to_register.choices = choices
-    
+
 def auto_register_b_and_rshg():
     '''
     Gets dirname from picker and populates the available timepoints to register.
@@ -168,28 +174,28 @@ def auto_register_b_and_rshg():
               dirname={'label':'Image region to load:','mode':'d'})
     def widget(
         dirname=Path.home(),
-        timepoints_to_register=(0),
+        timepoints_to_register=None,
         OVERWRITE: bool=False,
         ):
-    
+
         filelist = parse_unregistered_channels(dirname)
-        
+
         for t in progress(timepoints_to_register):
-        
+
             # Check for overwriting
             output_dir = path.split(path.dirname(filelist.loc[t,'R']))[0]
             if path.exists(path.join(path.dirname(filelist.loc[t,'R']),'R_reg_reg.tif'))  and not OVERWRITE:
             # and path.exists(path.join(path.dirname(B_tifs[t]),'B_reg_reg.tif'))  and not OVERWRITE:
                 print(f'Skipping t = {t} because its R_reg_reg.tif already exists')
                 continue
-                
+
             print(f'\n--- Started t = {t} ---')
             B = io.imread(filelist.loc[t,'B'])
             R_shg = io.imread(filelist.loc[t,'R_shg'])
             # G = io.imread(filelist.loc[t,'G'])
-            R = io.imread(filelist.loc[t,'R'])    
+            R = io.imread(filelist.loc[t,'R'])
             print('Done reading images')
-                
+
             # Find the slice with maximum mean value in R_shg channel
             z_ref = R_shg.mean(axis=2).mean(axis=1).argmax()
             print(f't = {t}: R_shg max std at {z_ref}')
@@ -198,7 +204,7 @@ def auto_register_b_and_rshg():
             z_moving = find_most_likely_z_slice_using_CC(R_ref,B)
             print(f'Cross correlation done and target Z-slice set at: {z_ref}')
             target = filters.gaussian(B[z_ref,...],sigma=0.5)
-        
+
             #NB: Here, move the R channel wrt the B channel
             print('StackReg + transform')
             sr = StackReg(StackReg.RIGID_BODY)
@@ -209,17 +215,17 @@ def auto_register_b_and_rshg():
             for i, R_slice in enumerate(R):
                 R_transformed[i,...] = warp(R_slice,T)
                 R_shg_transformed[i,...] = warp(R_shg[i,...],T)
-            
+
             # z-pad
             R_padded = z_translate_and_pad(B,R_transformed,z_ref,z_moving)
             R_shg_padded = z_translate_and_pad(B,R_shg_transformed,z_ref,z_moving)
-                
+
             output_dir = path.dirname(filelist.loc[t,'G'])
-        
+
             print('Saving')
             io.imsave(path.join(output_dir,'R_reg_reg.tif'),util.img_as_uint(R_padded/R_padded.max()),check_contrast=False)
             io.imsave(path.join(output_dir,'R_shg_reg_reg.tif'),util.img_as_uint(R_shg_padded/R_shg_padded.max()),check_contrast=False)
-        
+
     @widget.dirname.changed.connect
     def update_timepoints_on_file_change(event=None):
         _update_timepoints_on_file_change(widget)
@@ -240,7 +246,7 @@ def transform_image(
     rotate_theta:float=0.0,
     Transform_second_channel:bool=False,
     ) -> List[napari.layers.Layer]:
-    
+
     '''
     Perform 3D rigid-body transformations given an input image and manually set transformation parameters
     translate z/y/x: pixel-wise translations
@@ -250,7 +256,7 @@ def transform_image(
 
     Output will be the transformed image(s), with _transformed appended to name(s)
     '''
-    
+
     # Grab the image data + convert deg->radians
     image_data = image2transform.data.astype(float)
     second_image_data = second_channel.data.astype(float)
@@ -263,10 +269,10 @@ def transform_image(
     for z,im in enumerate(image_data):
         array[z,...] = warp(im, Txy)
     array = z_translate_and_pad(reference_image.data,array,reference_z,moving_z)
-    
+
     transformed_image = Image(array, name=image2transform.name+'_transformed', blending='additive', colormap=image2transform.colormap)
     output_list = [transformed_image]
-    
+
     if Transform_second_channel:
         array = np.zeros_like(second_image_data)
         for z,im in enumerate(second_image_data):
@@ -274,7 +280,7 @@ def transform_image(
         array = z_translate_and_pad(reference_image.data,array,reference_z,moving_z)
         transformed_second_channel = Image(array, name=second_channel.name+'_transformed', blending='additive', colormap=second_channel.colormap)
         output_list.append(transformed_second_channel)
-        
+
     return output_list
 
 @magicgui(call_button='Filter image')
@@ -300,8 +306,6 @@ viewer.window.add_dock_widget(auto_register_b_and_rshg(),area='left')
 viewer.window.add_dock_widget(LoadDTimepointForInspection(viewer),area='right')
 viewer.window.add_dock_widget(transform_image, area='right')
 viewer.window.add_dock_widget(filter_gaussian3d, area='right')
-
-viewer.window.add_dock_widget(print_image_size, area='left')
 
 if __name__ == '__main__':
     napari.run()
--- 2024_analysis/assemble_movie/napari_widget/imageLoadingWidgets.py
+++ 2024_analysis/assemble_movie/napari_widget/imageLoadingWidgets.py
@@ -43,14 +43,14 @@ class LoadDTimepointForInspection(Container):
                                   )
                     )
         self.append(PushButton(name='load_button', text='Load timepoint'))
-        
+
         self.dirname.changed.connect(self.update_timepoint_choices)
         self.timepoint2load.changed.connect(self.update_set_choices)
         self.load_button.changed.connect(self.load_images)
 
     def get_timepoint_choices(self, dropdown_widget):
         return self._timepoint_choices
-    
+
     def update_timepoint_choices(self):
         dirname = self.dirname.value
         choices = None
@@ -62,15 +62,15 @@ class LoadDTimepointForInspection(Container):
         if choices is not None:
             self._timepoint_choices = choices
             self.timepoint2load.reset_choices()
-            
+
     def get_set_choices(self, radio_widget):
         return self._set_choices
-    
+
     def update_set_choices(self):
         dirname = self.dirname.value
         t = self.timepoint2load.value
         subdir_name = glob(path.join(dirname,f'{t}. Day*/'))[0]
-        
+
         choices = []
         if len( glob(path.join(subdir_name,'R_reg.tif'))) > 0:
             choices.append('Un-registered')
@@ -80,26 +80,26 @@ class LoadDTimepointForInspection(Container):
             choices.append('Aligned')
         self._set_choices = choices
         self.set2load.reset_choices()
-        
-            
+
+
     def load_images(self) -> List[napari.layers.Layer]:
         # clear the current slate
         names2remove = [l.name for l in self._viewer.layers]
         for l in names2remove:
             self._viewer.layers.remove(l)
-                
+
         prefix = f'{self.timepoint2load.value}_'
-        
+
         if self.set2load.value == 'Un-registered':
             filelist = parse_unregistered_channels(self.dirname.value) #@todo: auto-detect when these are not yet available
-            suffix = '_reg_reg'
+            suffix = '_reg'
         elif self.set2load.value == 'Registered':
             filelist = parse_unaligned_channels(self.dirname.value)
-            suffix = '_reg'
+            suffix = '_reg_reg'
         elif self.set2load.value == 'Aligned':
             filelist = parse_aligned_timecourse_directory(self.dirname.value)
             suffix = '_align'
-            
+
         # Load the files using a cycling colormap series
         file_tuple = filelist.loc[self.timepoint2load.value]
         colormaps = cycle(['bop blue','gray','bop orange','bop purple'])
@@ -121,13 +121,13 @@ class LoadAlignedChannelForInspection(Container):
             ComboBox(name="channel2load", choices=self.get_channel_choices, label="Select channel")
         )
         self.append(PushButton(name='load_button', text='Load timepoint'))
-        
+
         self.dirname.changed.connect(self.update_channel_choices)
         self.load_button.changed.connect(self.load_images)
 
     def get_channel_choices(self, dropdown_widget):
         return self._channel_choices
-    
+
     def update_channel_choices(self):
         dirname = self.dirname.value
         choices = None
@@ -139,21 +139,21 @@ class LoadAlignedChannelForInspection(Container):
         if choices is not None:
             self._channel_choices = choices
             self.channel2load.reset_choices()
-        
-            
+
+
     def load_images(self) -> List[napari.layers.Layer]:
         # clear the current slate
         names2remove = [l.name for l in self._viewer.layers]
         for l in names2remove:
             self._viewer.layers.remove(l)
-        
+
         filelist = parse_aligned_timecourse_directory(self.dirname.value)
         file_tuple = filelist[self.channel2load.value]
-        
+
         stack = []
         for name,filename in file_tuple.items():
             stack.append(io.imread(filename))
             print(io.imread(filename).shape)
         stack = np.array(stack)
-        
+
         self._viewer.add_image(stack,name=self.channel2load.value+'_time_series', blending='additive', colormap='g')
