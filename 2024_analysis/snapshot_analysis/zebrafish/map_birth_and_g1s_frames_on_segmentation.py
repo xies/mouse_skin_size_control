a479c53179fa2c4f90ed11996a9eff521f48e60c
Shicong Xie <xies@users.noreply.github.com>
2024-09-17 13:55:10 -0700

    zebrafish CV done

--- 2024_analysis/snapshot_analysis/zebrafish/map_birth_and_g1s_frames_on_segmentation.py
+++ 2024_analysis/snapshot_analysis/zebrafish/map_birth_and_g1s_frames_on_segmentation.py
@@ -54,16 +54,21 @@ for tID, this_sIDs in spotsIDs_belonging_to_track.items():
     for sID in this_sIDs:
         _spots.loc[_spots['ID'] == sID,'TrackID'] = tID
 
-# Transfer the brith+G1S annotations
-birth_spots = pd.read_csv(path.join(dirname,'Position001_Mastodon/birth/spots.csv'),skiprows=[1,2])
+# Transfer the brith+G1S+div annotations
+birth_spots = pd.read_csv(path.join(dirname,'Position001_Mastodon/birth/birth-Spot.csv'),skiprows=[1,2])
 _spots['Birth'] = False
 for _,spot in birth_spots.iterrows():
     _spots.loc[_spots['ID'] == spot['ID'],'Birth'] = True
     
-g1s_spots = pd.read_csv(path.join(dirname,'Position001_Mastodon/g1s/spots.csv'),skiprows=[1,2])
+g1s_spots = pd.read_csv(path.join(dirname,'Position001_Mastodon/g1s/g1s-Spot.csv'),skiprows=[1,2])
 _spots['G1S'] = False
 for _,spot in g1s_spots.iterrows():
-    _spots.loc[_spots['ID'] == spot['ID'],'G1S'] = True             
+    _spots.loc[_spots['ID'] == spot['ID'],'G1S'] = True     
+
+div_spots = pd.read_csv(path.join(dirname,'Position001_Mastodon/division/division-Spot.csv'),skiprows=[1,2])
+_spots['G1S'] = False
+for _,spot in div_spots.iterrows():
+    _spots.loc[_spots['ID'] == spot['ID'],'Division'] = True            
 
 # Group out each track and sort
 birth_spots = _spots[_spots['Birth'] == True]
@@ -71,6 +76,9 @@ birth_spots = [t.sort_values('T') for _,t in birth_spots.groupby('TrackID')]
 
 g1s_spots = _spots[_spots['G1S'] == True]
 g1s_spots = [t.sort_values('T') for _,t in g1s_spots.groupby('TrackID')]
+
+div_spots = _spots[_spots['Division'] == True]
+div_spots = [t.sort_values('T') for _,t in div_spots.groupby('TrackID')]
 
 #%% Load segmentation files
 
@@ -124,16 +132,17 @@ for this_cell in tqdm(g1s_spots):
     # tID = int(this_cell.iloc[0]['TrackID'])
     current_trackID += 1
     
+    
     for _,this_frame in this_cell.iterrows():
         
         x = int(np.round(this_frame['X']))
         y = int(np.round(this_frame['Y']))
         z = int(np.round(this_frame['Z']))
         t = int(np.round(this_frame['T']))
-        im = io.imread(ven_files[t])
+        im = io.imread(mCh_files[t])
         label = im[z,y,x]
         if label == 0:
-            im = io.imread(mCh_files[t])
+            im = io.imread(ven_files[t])
             label = im[z,y,x]
         if label > 0:
             mask = im == label
@@ -147,4 +156,38 @@ for this_cell in tqdm(g1s_spots):
             g1s_img[t,z_low:z_high, y_low:y_high, x_low:x_high] = current_trackID
 
 io.imsave(path.join(dirname,'Position001_Mastodon/g1s/g1s_predicted.tif'), g1s_img)
+
+#%% division
+
+current_trackID = 0
+
+div_img = np.zeros((TT,ZZ,XX,XX),np.uint16)
+for this_cell in tqdm(div_spots):
+    # tID = int(this_cell.iloc[0]['TrackID'])
+    current_trackID += 1
+    
+    
+    for _,this_frame in this_cell.iterrows():
+        
+        x = int(np.round(this_frame['X']))
+        y = int(np.round(this_frame['Y']))
+        z = int(np.round(this_frame['Z']))
+        t = int(np.round(this_frame['T']))
+        im = io.imread(mCh_files[t])
+        label = im[z,y,x]
+        if label == 0:
+            im = io.imread(ven_files[t])
+            label = im[z,y,x]
+        if label > 0:
+            mask = im == label
+            div_img[t,mask] = current_trackID
+        else:
+            # Create a 'cube' around spots that are missing segmentations
+            # print(f'Time {t} -- {i}: {ID}')
+            y_low = max(0,y - radius); y_high = min(XX,y + radius)
+            x_low = max(0,x - radius); x_high = min(XX,x + radius)
+            z_low = max(0,z - radius); z_high = min(ZZ,z + radius)
+            div_img[t,z_low:z_high, y_low:y_high, x_low:x_high] = current_trackID
+
+io.imsave(path.join(dirname,'Position001_Mastodon/division/div_predicted.tif'), div_img)
 
--- 2024_analysis/snapshot_analysis/zebrafish/quantify_cv.py
+++ 2024_analysis/snapshot_analysis/zebrafish/quantify_cv.py
@@ -13,6 +13,9 @@ from os import path
 from glob import glob
 from skimage import io, measure
 from tqdm import tqdm
+from scipy.stats import variation
+import matplotlib.pyplot as plt
+from mathUtils import cvariation_bootstrap, cv_difference_pvalue
 
 import xml.etree.ElementTree as ET
 
@@ -20,31 +23,77 @@ dirname = '/Users/xies/Library/CloudStorage/OneDrive-Stanford/In vitro/CV from s
 
 #%%
 
-birth = io.imread(path.join(dirname,'Position001_Mastodon/birth/birth_manual.tif'))
+def quantify_volume(f):
+    
+    im = io.imread(f)
+    
+    _df = []
+    for t in tqdm(range(im.shape[0])):
+        this_df = pd.DataFrame(measure.regionprops_table(im[t,...],properties=['label','area']))
+        this_df['Frame'] = t
+        _df.append(this_df)
+    _df = pd.concat(_df)
+    _df = _df.rename(columns={'area':'Volume'})
+    _df = pd.DataFrame(_df.groupby('label')['Volume'].mean())
 
-_df = []
-for t in tqdm(range(birth.shape[0])):
-    this_df = pd.DataFrame(measure.regionprops_table(birth[t,...],properties=['label','area']))
-    this_df['Frame'] = t
-    _df.append(this_df)
-_df = pd.concat(_df)
-_df = _df.rename(columns={'area':'Volume'})
+    return _df
 
-df = pd.DataFrame(_df.groupby('label')['Volume'].mean())
-df['Phase'] = 'Birth'
+birth = quantify_volume(path.join(dirname,'Position001_Mastodon/birth/birth_manual.tif'))
+birth['Phase'] = 'Birth'
+g1s = quantify_volume(path.join(dirname,'Position001_Mastodon/g1s/g1s_manual.tif'))
+g1s['Phase'] = 'G1S'
+div = quantify_volume(path.join(dirname,'Position001_Mastodon/division/div_manual.tif'))
+div['Phase'] = 'Division'
+df = pd.concat((birth,g1s,div),ignore_index=True)
 
-g1s = io.imread(path.join(dirname,'Position001_Mastodon/g1s/g1s_manual.tif'))
+# birth = io.imread(path.join(dirname,'Position001_Mastodon/birth/birth_manual.tif'))
 
-_df = []
-for t in tqdm(range(g1s.shape[0])):
-    this_df = pd.DataFrame(measure.regionprops_table(g1s[t,...],properties=['label','area']))
-    this_df['Frame'] = t
-    _df.append(this_df)
-_df = pd.concat(_df)
-_df = _df.rename(columns={'area':'Volume'})
-_df = pd.DataFrame(_df.groupby('label')['Volume'].mean())
-_df['Phase'] = 'G1S'
+# _df = []
+# for t in tqdm(range(birth.shape[0])):
+#     this_df = pd.DataFrame(measure.regionprops_table(birth[t,...],properties=['label','area']))
+#     this_df['Frame'] = t
+#     _df.append(this_df)
+# _df = pd.concat(_df)
+# _df = _df.rename(columns={'area':'Volume'})
 
-df = pd.concat((df,_df),ignore_index=True)
+# df = pd.DataFrame(_df.groupby('label')['Volume'].mean())
+# df['Phase'] = 'Birth'
+
+# g1s = io.imread(path.join(dirname,'Position001_Mastodon/g1s/g1s_manual.tif'))
+
+# _df = []
+# for t in tqdm(range(g1s.shape[0])):
+#     this_df = pd.DataFrame(measure.regionprops_table(g1s[t,...],properties=['label','area']))
+#     this_df['Frame'] = t
+#     _df.append(this_df)
+# _df = pd.concat(_df)
+# _df = _df.rename(columns={'area':'Volume'})
+# _df = pd.DataFrame(_df.groupby('label')['Volume'].mean())
+# _df['Phase'] = 'G1S'
+
+# df = pd.concat((df,_df),ignore_index=True)
 
 #%%
+
+
+CV = pd.DataFrame()
+
+for phase,_df in df.groupby('Phase',sort=False):
+    cv,lb,ub = cvariation_bootstrap(_df['Volume'].values,Nboot = 10000,subsample=100)
+    CV.loc[phase,'CV'] = cv
+    CV.loc[phase,'ub'] = ub
+    CV.loc[phase,'lb'] = lb
+    
+plt.errorbar([1,2,3],CV['CV'],yerr = (CV['ub'] - CV['lb'])/2)
+plt.ylabel('CV in nuclear size +/- 95% interval via bootstrap')
+plt.xticks([1,2,3],labels=['Birth','G1/S','Division'])
+
+print(CV)
+
+#%% Calculate P-values
+
+cv_difference_pvalue(df[df['Phase'] == 'Birth']['Volume'],df[df['Phase'] == 'G1S']['Volume'], Nboot=10000)
+cv_difference_pvalue(df[df['Phase'] == 'Division']['Volume'],df[df['Phase'] == 'G1S']['Volume'], Nboot=10000)
+
+    
+
