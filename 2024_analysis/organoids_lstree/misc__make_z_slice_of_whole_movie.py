7286ceb2dfce8ef648221ecad239445db9aebdfb
Shicong Xie <xies@users.noreply.github.com>
2025-01-07 15:51:36 -0800

    fixed organoid MIP bug

--- 2024_analysis/organoids_lstree/misc__make_z_slice_of_whole_movie.py
+++ 2024_analysis/organoids_lstree/misc__make_z_slice_of_whole_movie.py
@@ -42,9 +42,9 @@ for t,(h2b,cdt,gem) in enumerate(zip(c00,c01,c02)):
     cdt = io.imread(cdt)
     gem = io.imread(gem)
     
-    stack[0,t,...] = h2b.sum(axis=0)
-    stack[1,t,...] = cdt.sum(axis=0)
-    stack[2,t,...] = gem.sum(axis=0)
+    stack[0,t,...] = h2b.max(axis=0)
+    stack[1,t,...] = cdt.max(axis=0)
+    stack[2,t,...] = gem.max(axis=0)
     
 io.imsave('/Users/xies/Desktop/mip00.tif', stack[0,...])
 io.imsave('/Users/xies/Desktop/mip01.tif', stack[1,...])
@@ -55,33 +55,31 @@ io.imsave('/Users/xies/Desktop/mip02.tif', stack[2,...])
 
 from tqdm import tqdm
 
-dirname ='/Volumes/Seagate Backup Plus Drive/20200303_194709_09/14_crop/object_0/'
+dirname ='/Volumes/Seagate Backup Plus Drive/20200303_194709_09/14_crop/'
 
-c00 = natsort.natsorted(glob(path.join(dirname,'FUCCI*.tif')))[:200]
-c01 = natsort.natsorted(glob(path.join(dirname,'Lgr5GFP*.tif')))[:200]
+T = 200
+c00 = natsort.natsorted(glob(path.join(dirname,'decon/FUCCIC/FUCCI*.tif')))[:T]
+c01 = natsort.natsorted(glob(path.join(dirname,'decon/Lgr5GFP/Lgr5GFP*.tif')))[:T]
 
 z2slice = 120
 
-stack = np.zeros((2,200,932,1064))
+stack = np.zeros((2,T,500,420),dtype=np.int32)
+MIP = np.zeros((2,T,500,420),dtype=np.int32)
 for t,(h2b,cdt) in tqdm(enumerate(zip(c00,c01))):
    
     h2b = io.imread(h2b)
     cdt = io.imread(cdt)
     
-    stack[0,t,...] = h2b[z2slice,...]
-    stack[1,t,...] = cdt[z2slice,...]
+    stack[0,t,...] = h2b[z2slice,:500,:420]
+    stack[1,t,...] = cdt[z2slice,:500,:420]
+
+    MIP[0,t,...] = h2b[:,:500,:420].max(axis=0)
+    MIP[1,t,...] = cdt[:,:500,:420].max(axis=0)
     
 io.imsave('/Users/xies/Desktop/ch00.tif', stack[0,...])
 io.imsave('/Users/xies/Desktop/ch01.tif', stack[1,...])
 
-stack = np.zeros((2,200,404,396))
-for t,(h2b,cdt,gem) in enumerate(zip(c00,c01,c02)):
-   
-    h2b = io.imread(h2b)
-    cdt = io.imread(cdt)
-    
-    stack[0,t,...] = h2b.sum(axis=0)
-    stack[1,t,...] = cdt.sum(axis=0)
-    
-io.imsave('/Users/xies/Desktop/mip00.tif', stack[0,...])
-io.imsave('/Users/xies/Desktop/mip01.tif', stack[1,...])
+
+io.imsave('/Users/xies/Desktop/mip00.tif', MIP[0,...])
+io.imsave('/Users/xies/Desktop/mip01.tif', MIP[1,...])
+
